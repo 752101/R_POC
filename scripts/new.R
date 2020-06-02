0c3eebebca20f7021ad5e7b6eeae19cf0b8ea07a
752101 <31843508+752101@users.noreply.github.com>
2020-06-02 09:26:44 +0530

    Add files via upload

---
+++ scripts/new.R
@@ -0,0 +1,14 @@
+needs(ggplot2)
+needs(readxl)
+needs(ggthemes)
+attach(input[[1]])
+data<-read_excel(first)
+View(data)
+
+library(tcltk)
+
+windows()     #Use X11() or quartz() if on linux or mac.
+ggplot(data,aes(x=data$Volume)) + geom_line(aes(y=data$Conductivity))+xlab('Volume')+ylab('Mean')+theme_gdocs()
+prompt  <- "hit spacebar to close plots"
+extra   <- "some extra comment"
+capture <- tk_messageBox(message = prompt, detail = extra)
